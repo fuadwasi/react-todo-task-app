3edb973c526cfaa5792774656ca16006c467e141
fuadwasi <fuad@bssitbd.com>
2021-02-08 20:02:23 +0600

    Id added

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,11 +2,13 @@ import {FaTimes} from 'react-icons/fa'
 const Task = ({task, onDelete}) => {
     return (
         <div className="task">
+            <h4>{task.id}</h4>
             <h3>
                 {task.text} 
                 <FaTimes onClick={onDelete} style={{color:'black', cursor:'pointer'}}/>
             </h3>
             <p>{task.day}</p>
+            
 
         </div>
     )
