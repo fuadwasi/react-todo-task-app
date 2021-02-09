02e91ca7532dd71c822782541d21fa65d8005c73
fuadwasi <fuad15-9400@diu.edu.bd>
2021-02-09 20:56:21 +0600

    npm json-server added

--- src/App.js
+++ src/App.js
@@ -8,24 +8,7 @@ import AddTask from './components/AddTask'
 function App() {
   const [showAddTask, setShowAddTask]=useState(false)
   const [tasks, setTasks]= useState([
-    { 
-        id: 1,
-        text:"Doctor dake",
-        day: "Feb 5th at 2:30pm",
-        reminder: true,
-    },
-    { 
-        id: 2,
-        text:"Meeting at school",
-        day: "Feb 6th at 4:30pm",
-        reminder: true,
-    },
-    { 
-        id: 3,
-        text:"Gumaite Jaw",
-        day: "Feb 7th at 7:30pm",
-        reminder: false,
-    },
+    
 ]
 )
 
